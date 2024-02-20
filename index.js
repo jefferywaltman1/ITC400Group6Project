863fc1a6ae458b442ce7f3e43be31191e71b938c
jefferywaltman1 <159663790+jefferywaltman1@users.noreply.github.com>
2024-02-20 18:17:44 +0000

    howtoplayDev1

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '/index.html';
     });
 });
+// Navigate to How To Play Screen
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('LtoP').addEventListener('click', function() {
+        window.location.href = '/HowToPlay.html';
+    });
+});
