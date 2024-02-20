5531fa17a699aa7e54e5aac88d118a849df37d6e
jefferywaltman1 <159663790+jefferywaltman1@users.noreply.github.com>
2024-02-20 15:54:42 +0000

    AddedLoginPage

---
+++ index.js
@@ -0,0 +1,12 @@
+// Navigate to Login Screen
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('LoginBox').addEventListener('click', function() {
+        window.location.href = '/LoginScreen.html';
+    });
+});
+// Navigate to Landing Screen
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('DL_ICON').addEventListener('click', function() {
+        window.location.href = '/index.html';
+    });
+});
